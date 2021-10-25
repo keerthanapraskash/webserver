ba54392bd741b53d864f1b355a1dc37470acb5df
keerthanapraskash <keerthanaprakash7403@gmail.com>
2021-10-25 14:01:36 +0530

    Implemented the serach feature

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,40 @@
 console.log("client side js file is loaded");
 
-fetch('http://localhost:3000/weather?address=alappuzha').then((Response)=>{
-    Response.json().then((data)=>{
-        if(data.error){
-            console.log(data.error)
-        }else{
-            console.log(data.forcast);
-            console.log(data.placeName)
-            console.log(data);
 
-        }
-        
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+
+
+
+weatherForm.addEventListener('submit',(e)=>{
+
+    e.preventDefault();
+    
+    const location = search.value;
+
+    messageOne.textContent = 'Loading....'
+    messageTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address='+location).then((Response)=>{
+        Response.json().then((data)=>{
+            if(data.error){
+                console.log(data.error)
+                messageOne.textContent = data.error;
+            }else{
+                console.log(data.forcast);
+                console.log(data.placeName)
+                console.log(data);
+
+                messageOne.textContent = data.placeName;
+                messageTwo.textContent = data.forcast;
+    
+            }
+            
+        })
     })
-})
+
+    
+});
